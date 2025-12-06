12cbed5fd9e49bca7e62b39979301ba1d31896fe
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-12-06 08:19:18 +0100

    syling cart component

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,5 +1,6 @@
 import "./Cart.css";
 import cartLogo from "./assets/images/illustration-empty-cart.svg";
+import carbonLogo from "./assets/images/icon-carbon-neutral.svg"
 
 function Cart({ cart }) {
   const items = Object.values(cart);
@@ -21,14 +22,24 @@ function Cart({ cart }) {
           {items.map((item, i) => (
             <div key={i} className="cart-item">
               <p>{item.name}</p>
-              <p>Qty: {item.qty}</p>
-              <p>${(item.qty * item.price).toFixed(2)}</p>
+              <div className="cart-price">
+                  <p>{item.qty}x</p>
+                  <p>@{item.price.toFixed(2)}</p>
+                  <p>${(item.qty * item.price).toFixed(2)}</p>
+              </div>
             </div>
           ))}
 
           <hr />
+          <div className="total">
+            <p>Order Total</p>
+            <h4>${total.toFixed(2)}</h4>
+          </div>
 
-          <h4>Total: ${total.toFixed(2)}</h4>
+          <div className="carbon">
+            <img src={carbonLogo} alt=""></img>
+            <p>This is a carbon-neutral delivery</p>
+          </div>
         </>
       )}
     </div>
